828968eaabb4078446e78638a75f7eacfc0effab
mattmcburney <72657850+mattmcburney@users.noreply.github.com>
2021-10-11 10:49:55 -0400

    Create BoolTest1.java

---
+++ Unit 3/BoolTest1.java
@@ -0,0 +1,15 @@
+public class BoolTest1
+{
+   public static void main(String[] args)
+   {
+     int age = 15;
+     int year = 14;
+     // Will this print true or false?
+     System.out.println( age == year );
+     year = 15;
+     // Will this print true or false?
+     System.out.println( age == year );
+     // Will this print true or false?
+     System.out.println( age != year );
+   }
+}
